fdf205d07f0f07b87f282b93d8b7a5d46f390d90
Julian Krese <138831029+JulianKrese@users.noreply.github.com>
2025-05-09 14:53:41 -0400

    Add files via upload

---
+++ final/discord_bot.py
@@ -0,0 +1,83 @@
+import requests
+import discord
+import pandas as pd
+from discord.ext import commands
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+# discord setup
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents, case_insensitive=True)
+
+
+# gemini API requests to answer user questions
+async def send_flask_chat(question):
+
+    # gemini API calld
+    url = f"http://34.48.125.135:5000/chat"  # GCP flask API endpoint
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "message": question
+    }
+
+    try:
+        # get gemini's response to the query
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        result = response.json()
+        return result["reply"]
+    except Exception as e:
+        print(f"Gemini API error: {e}")
+        return "Sorry, I need to take a quick break. Give me a sec and then ask again!"
+
+
+# for chunking message into discord channel
+async def send_long_message(channel, content):
+    for i in range(0, len(content), 2000):
+        await channel.send(content[i:i+2000])
+
+
+# commmands
+bot.remove_command("help")
+@bot.command()
+async def help(ctx):
+    await ctx.send(f"Hello {ctx.author.name}! " \
+                   "I am a chat bot, George, here to help answer all your questions! " \
+                   "Although most bots lack current data, I am specialized in the masters, so ask away!")
+
+
+# events
+@bot.event
+async def on_ready():
+    print("Bot is running...")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Uh-oh, I don't know that command! Try '!help' to see how to interact with me!")
+
+@bot.event
+async def on_message(message):
+    # global message_num, user_messages, bot_messages
+    if message.author == bot.user:
+        return
+    
+    modified_message = "Here is the author of a query and the question: " + str(message.author) + " : " + message.content
+    gemini_response = await send_flask_chat(modified_message)
+
+    # message_num += 1
+    # user_messages[message_num] = message
+    # bot_messages[message_num] = gemini_response
+
+    await send_long_message(message.channel, gemini_response)
+            
+    await bot.process_commands(message)
+    
+bot.run(BOT_TOKEN)
